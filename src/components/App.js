6953e543eaf92b81b895057dacd24f895ce50977
Melissa Faulner <melissa.faulner@gmail.com>
2021-03-01 17:03:41 -0500

    commit tree 8495f02d2a3abcbc4d153779dbe2bbac664c7564

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listingData, setListingData] = useState([])
+
+  const URL = "http://localhost:6001/listings"
+
+  useEffect(() => {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((response) => setListingData(response))
+  }, []);
+
+  function handleDeleteClick(e) {
+    const itemId = parseInt(e.target.value)
+
+    fetch(URL + `/${itemId}`, { method: "DELETE" });
+    
+    const updatedData = listingData.filter ((listing) => listing.id !== itemId)
+    setListingData(updatedData);
+  };
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listingData} handleDeleteClick={handleDeleteClick} />
     </div>
   );
 }
