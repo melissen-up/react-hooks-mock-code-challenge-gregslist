b5f57baa34f3b0a7a24e399821d9340577628d91
Melissa Faulner <melissa.faulner@gmail.com>
2021-03-01 17:04:05 -0500

    core deliverables complete

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({ image="https://via.placeholder.com/300x300", description, location, id, handleDeleteClick}) {
+  const [favorite, setFavorite] = useState(false)
+
+  function handleFavorite() {
+    setFavorite((favorite) => !favorite)
+  };
+
   return (
     <li className="card">
-      <div className="image">
+      <div className={"image"}>
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {favorite ? (
+          <button onClick={handleFavorite} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={handleFavorite} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button onClick={handleDeleteClick} value={id} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
